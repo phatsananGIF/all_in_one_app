d13330d0c3213e3883ab09aff184a1bbb17f4bd1
gifpat <gitpat@gmail.com>
2021-08-21 18:37:21 +0700

    CalculatorPage

---
+++ lib/screen/my_calculator_page/my_calculator_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyCalculatorPage extends StatelessWidget {
+  const MyCalculatorPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("data"),
+      ),
+      body: Text("data"),
+    );
+  }
+}
